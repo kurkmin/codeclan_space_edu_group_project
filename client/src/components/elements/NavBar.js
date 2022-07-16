07568d80ade8d0085a18ddfc13d74588cd7aee13
James R <jimmer1609@gmail.com>
2022-07-16 20:27:56 +0900

    fleshed out header bar

--- client/src/components/elements/NavBar.js
+++ client/src/components/elements/NavBar.js
@@ -3,7 +3,11 @@ import React from 'react';
 const NavBar = () => {
   return (
     <>
-      <h2>NavBar</h2>
+      <ul className='navigation'>
+        <li><a href='/'>Home Page</a></li>
+        <li><a href='/explore'>Explore</a></li>
+        <li><a href='/quizzes'>Quiz</a></li>
+      </ul>
     </>
   )
 }
