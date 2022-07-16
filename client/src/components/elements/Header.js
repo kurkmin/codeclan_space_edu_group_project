0303ed8950ab221aa92410b06fa2a0bfa6e54bee
Min <min@Minui-Macmini.local>
2022-07-16 22:06:20 +0100

    user dropdown menu added

--- client/src/components/elements/Header.js
+++ client/src/components/elements/Header.js
@@ -3,12 +3,12 @@ import React from 'react';
 import NavBar from './NavBar';
 import UserDetail from './UserDetails';
 
-const Header = () => {
+const Header = ({ users }) => {
   return (
     <header>
       <h1>TOP LEVEL</h1>
       <NavBar />
-      <UserDetail />
+      <UserDetail users={users} />
     </header>
   )
 }
