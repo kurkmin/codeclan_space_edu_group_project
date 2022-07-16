13f08299f70dd243f1941a08811f9e173c67d06f
Student <student@Students-MacBook-Pro.local>
2022-07-16 13:43:26 +0100

    please work

--- client/src/containers/QuizContainer.js
+++ client/src/containers/QuizContainer.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Answers from '../components/quiz/Answers';
 import Questions from '../components/quiz/Questions';
 
-function QuizContainer() {
+function QuizContainer( {planets }) {
+
+  const [quizzes, setQuizzes] = useState([]);
+
+  async function getQuizzes() {
+    const url = 'https://localhost:9000/api/quizzes';
+    const response = await fetch(url)
+    const data = await response.json();
+    setQuizzes(data)
+    console.log(quizzes)
+
+  }
 
   return (
     <>
       <h1>I'm the quiz container</h1>
-      <Questions />
+      <Questions quizzes={quizzes}/>
       <Answers />
     </>
   )
