b296a33e92180049c11bab5807ca4022c47e97ad
GM1WKR <gm1wkr@gmail.com>
2022-07-17 16:04:36 +0100

    Add globalStyles

---
+++ client/src/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: #121212;
+    color: #fefefe;
+  }
+`;
+
+export default GlobalStyle;
