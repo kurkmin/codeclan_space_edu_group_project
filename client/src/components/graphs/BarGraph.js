bfe215128e9e12187894bddd6e1e26932539e4cd
James R <jimmer1609@gmail.com>
2022-07-18 23:20:40 +0900

    more info and experimentation with graphs

--- client/src/components/graphs/BarGraph.js
+++ client/src/components/graphs/BarGraph.js
@@ -19,24 +19,41 @@ ChartJS.register(
   Legend
 );
 
-function BarGraph() {
+function BarGraph({planets}) {
+
+  const planetNames = planets.map(planet => planet.englishName);
+  const planetGravities = planets.map(planet => planet.gravity);
 
   const data = {
-    labels: ['Jan', 'Feb'],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [65, 70]
-    }]
+    labels: [...planetNames],
+    datasets: [
+      {
+      label: 'Gravity',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [...planetGravities]
+    },
+    // {
+    //   label: "Your Weight",
+    //   type: "line",
+    //   borderColor: "#8e5ea2",
+    //   data: [4,6,8,10,12,10,8,10]
+    // }
+  ]
   };
 
   return (
   <div>
-    <h2>space bar</h2>
+    <h2>Space Stats:-</h2>
     <Bar 
+      type='bar'
       data={data}
       width={100}
       height={50}
-      options={{maintainAspectRatio: false}}
+      options={{maintainAspectRatio: true}}
     />
   </div>
 )
