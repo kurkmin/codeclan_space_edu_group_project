14a105124768ac61905b7e488ec73048e7162fb6
James R <jimmer1609@gmail.com>
2022-07-19 14:39:57 +0900

    fixed bugs added navbar added moons

--- client/src/components/graphs/BarGraph.js
+++ client/src/components/graphs/BarGraph.js
@@ -1,84 +1,9 @@
 import React from "react";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  PointElement,
-  LineElement,
-} from 'chart.js';
-
-
+import {Chart as ChartJS} from 'chart.js/auto';
 import { Bar } from 'react-chartjs-2';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  PointElement,
-  LineElement
-);
 
-function BarGraph({planets, users}) {
-
-  const planetNames = planets.map(planet => planet.englishName);
-  const planetGravities = planets.map(planet => planet.gravity);
-  const userweight = users[0].mass
-  const userMassPerPlanet = planets.map(planet =>  (userweight/9.81) * planet.gravity)
-
-  const data = {
-    labels: [...planetNames],
-    datasets: [
-      {
-      label: 'Gravity',
-      type: 'bar',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [...planetGravities]
-    },
-    {
-      label: 'Your Weight',
-      type: 'line',
-      borderColor: "#8e5ea2",
-      data: [...userMassPerPlanet]
-    }
-  ],
-  // ,
-  // options: {
-  //   scales: {
-  //     A: {
-  //         id: 'A',
-  //         type: 'linear',
-  //         position: 'left',
-  //         scaleLabel: {
-  //           display: true,
-  //           labelString: 'G',
-  //           beginAtZero: true,
-  //         }
-  //       },
-  //     B: {
-  //         id: 'B',
-  //         type: 'linear',
-  //         position: 'right',
-  //         scaleLabel: {
-  //           display: true,
-  //           labelString: 'Mass',
-  //           beginAtZero: true,
-  //         }
-  //       }
-      
-  //   }
-  // }
-  };
+function BarGraph({data, options}) {
 
   return (
   <div>
@@ -88,12 +13,10 @@ function BarGraph({planets, users}) {
       data={data}
       width={100}
       height={50}
-      options={{maintainAspectRatio: true}}
+      options={options}
     />
   </div>
 )
-
 }
-
 
 export default BarGraph;
--- client/src/containers/StatisticsContainer.js
+++ client/src/containers/StatisticsContainer.js
@@ -1,13 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 
 import BarGraph from '../components/graphs/BarGraph';
 
 function StatisticsContainer({planets, users}) {
+  const planetNames = planets.map(planet => planet.englishName)
+  // const userweight = users[0].mass
+  // const userMassPerPlanet = planets.map(planet => (userweight / 9.81) * planet.gravity)
+
+  const [gravityData, setGravityData] = useState({
+    labels: [...planetNames],
+    datasets: [{
+      label: 'Gravity',
+      type: 'bar',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: planets.map(planet => planet.gravity)
+    }]
+  });
+  const gravityOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Gravities in our Solar System',
+        position: 'bottom',
+      },
+    },
+  };
+
+  const [planetMoons, setPlanetMoons] = useState({
+    labels: [...planetNames],
+    datasets: [{
+      label: 'Moons',
+      type: 'bar',
+      backgroundColor: 'rgba(0,0,200,0.2)',
+      borderColor: 'rgba(0,0,200,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(0,0,200,0.4)',
+      hoverBorderColor: 'rgba(0,0,200,1)',
+      data: planets.map(planet => planet.moons ? planet.moons.length : 0)
+    }]
+  });
+  const moonOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Moons on each of the Planets',
+        position: 'bottom',
+      },
+    },
+  };
 
   return (
     <>
-      <h1>I'm the container</h1>
-      <BarGraph planets={planets} users={users}/>
+      <BarGraph data={gravityData} options={gravityOptions}/>
+      <BarGraph data={planetMoons} options={moonOptions}/>
     </>
   )
 
